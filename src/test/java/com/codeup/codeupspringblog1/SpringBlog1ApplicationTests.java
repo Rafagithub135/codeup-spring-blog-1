2ab9874146322ae3ccc0befad01262b182683955
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-02-27 17:10:40 -0500

    recreating spring blog repository

---
+++ src/test/java/com/codeup/codeupspringblog1/SpringBlog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlog1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
